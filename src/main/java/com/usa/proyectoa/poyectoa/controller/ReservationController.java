5f50adeba22785d02cacb2b118f5933d25d0da68
kathering <kathering.rivera.mt@usa.edu.co>
2022-10-03 18:47:03 -0500

    v1.0.5

--- src/main/java/com/usa/proyectoa/poyectoa/controller/ReservationController.java
+++ src/main/java/com/usa/proyectoa/poyectoa/controller/ReservationController.java
@@ -4,6 +4,7 @@ package com.usa.proyectoa.poyectoa.controller;
 import com.usa.proyectoa.poyectoa.entities.Reservation;
 import com.usa.proyectoa.poyectoa.service.ReservationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,6 +22,7 @@ public class ReservationController {
     }
 
     @PostMapping("/save")
+    @ResponseStatus(HttpStatus.CREATED)
     public Reservation save(@RequestBody Reservation a){
         return reservationService.save(a);
     }
